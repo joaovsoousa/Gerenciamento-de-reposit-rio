45295a45a21d7ee2194b68e090d8ae05c6e07bbe
joao <jvsjoaovitordesousa1@gmail.com>
2021-06-24 14:27:43 -0300

    Noturno

---
+++ assets/java/app.js
@@ -0,0 +1,66 @@
+var exibirTeste1 = function(){
+    divTeste1 = document.querySelector('#teste1');
+    divTeste2 = document.querySelector('#teste2');
+    divTeste3 = document.querySelector('#teste3');
+    divTeste4 = document.querySelector('#teste4');
+
+    divTeste1.style.display = "block";
+    divTeste2.style.display = "none";
+    divTeste3.style.display = "none";
+    divTeste4.style.display = "none";
+    divTeste5.style.display = "none";
+
+}
+var exibirTeste2 = function(){
+    divTeste1 = document.querySelector('#teste1');
+    divTeste2 = document.querySelector('#teste2');
+    divTeste3 = document.querySelector('#teste3');
+    divTeste4 = document.querySelector('#teste4');
+
+    divTeste1.style.display = "none";
+    divTeste2.style.display = "block";
+    divTeste3.style.display = "none";
+    divTeste4.style.display = "none";
+    divTeste5.style.display = "none";
+
+}
+var exibirTeste3 = function(){
+    divTeste1 = document.querySelector('#teste1');
+    divTeste2 = document.querySelector('#teste2');
+    divTeste3 = document.querySelector('#teste3');
+    divTeste4 = document.querySelector('#teste4');
+
+    divTeste1.style.display = "none";
+    divTeste2.style.display = "none";
+    divTeste3.style.display = "block";
+    divTeste4.style.display = "none";
+    divTeste5.style.display = "none";
+
+}
+var exibirTeste4 = function(){
+    divTeste1 = document.querySelector('#teste1');
+    divTeste2 = document.querySelector('#teste2');
+    divTeste3 = document.querySelector('#teste3');
+    divTeste4 = document.querySelector('#teste4');
+
+    divTeste1.style.display = "none";
+    divTeste2.style.display = "none";
+    divTeste3.style.display = "none";
+    divTeste4.style.display = "block";
+    divTeste5.style.display = "none";
+
+}
+var exibirTeste5 = function(){
+    divTeste1 = document.querySelector('#teste1');
+    divTeste2 = document.querySelector('#teste2');
+    divTeste3 = document.querySelector('#teste3');
+    divTeste4 = document.querySelector('#teste4');
+    divTeste5 = document.querySelector('#teste5');
+
+    divTeste1.style.display = "none";
+    divTeste2.style.display = "none";
+    divTeste3.style.display = "none";
+    divTeste4.style.display = "none";
+    divTeste5.style.display = "block";
+
+}
